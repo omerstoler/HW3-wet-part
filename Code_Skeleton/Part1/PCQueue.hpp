5c15ca16b187471555d598ba1613b48e8d801a38
omerstoler <31178662+omerstoler@users.noreply.github.com>
2019-05-17 16:51:31 +0300

    Omer: cpp implementation into hpp - linker problems

--- Code_Skeleton/Part1/PCQueue.hpp
+++ Code_Skeleton/Part1/PCQueue.hpp
@@ -8,17 +8,29 @@ template <typename T>class PCQueue
 
 public:
 	// default constructor
-	PCQueue();
+	PCQueue() : queue_lock(1), available_items() {};
 
 	// Blocks while queue is empty. When queue holds items, allows for a single
 	// thread to enter and remove an item from the front of the queue and return it.
 	// Assumes multiple consumers.
-	T pop();
+	T pop(){
+		T item;
+		queue_lock.down();
+		available_items.down();
+		item = q.pop();
+		queue_lock.up();
+		return item;
+	};
 
 	// Allows for producer to enter with *minimal delay* and push items to back of the queue.
 	// Hint for *minimal delay* - Allow the consumers to delay the producer as little as possible.
 	// Assumes single producer
-	void push(const T& item);
+	void push(const T& item){
+		queue_lock.down();
+		q.push(item);
+		available_items.up();
+		queue_lock.up();
+	};
 
 
 private:
